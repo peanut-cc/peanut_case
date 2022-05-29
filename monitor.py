934d415a277aad9a146a9f9fd7f28af18c4ef9f5
fan.zhao <hjzhaofan@gmail.com>
2022-05-29 23:01:58 +0800

    fix: 🐛 修复windows 连续快速复制同一个文件时提示权限的bug

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 # 监听每个客户的 uploads 目录 用于通知其他开始处理
 import os
 
+from uuid import uuid4
 from watchdog.events import FileSystemEventHandler
 import logging
 
@@ -30,5 +31,12 @@ class FileMonitor(FileSystemEventHandler):
         if not event.is_directory:
             file_path = os.path.dirname(event.src_path)
             dir_name = os.path.basename(file_path)
-            self.callback(dir_name, event.src_path)
+            file_name = os.path.basename(event.src_path)
+            if ".csv" in file_name:
+                new_name = "{}/{}.csv".format(file_path, uuid4())
+            else:
+                new_name = "{}/{}.xlsx".format(file_path, uuid4())
+            logging.info("custom {0} upload file {1}".format(dir_name,event.src_path))
+            os.rename(event.src_path, new_name)
+            self.callback(dir_name, new_name)
 
